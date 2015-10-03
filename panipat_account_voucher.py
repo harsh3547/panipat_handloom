623d543657831c9e56958d0d8add23dfffa9bda7
Harsh Jain <harsh.jain@jginfosystems.com>
2015-10-03 16:38:05 +0530

    vals passed only when context has crm_lead_id

--- panipat_account_voucher.py
+++ panipat_account_voucher.py
@@ -6,8 +6,12 @@ class account_voucher(osv.osv):
     def create(self,cr,uid,vals,context=None):
         
         print "context-----------------------------",context,vals
-        vals['crm_lead_id'] = context.get('crm_lead_id',False)
-        vals['type']= 'receipt'
+        if context.get('crm_lead_id',False):
+            vals['crm_lead_id'] = context.get('crm_lead_id',False)
+            vals['type'] = 'receipt'
+            vals['partner_id'] = context.get('default_partner_id',False)
+            # so that even if user changes the partner it'll still record payment of partner 
+            # of corresponding order from panipat.crm.lead
         print "vals-----------------------------",vals
         return super(account_voucher,self).create(cr,uid,vals,context=None)
     
