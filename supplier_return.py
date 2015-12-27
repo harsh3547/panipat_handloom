4ad18f91f5a35f7ea02c6a6549b9b4fe5265af62
harsh jain <harsh.jain@jginfosystems.com>
2015-12-27 19:33:54 +0530

    Adding supplier return files

---
+++ supplier_return.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+from openerp.exceptions import except_orm
+from datetime import datetime
+
+class panipat_supplier_return(models.Model):
+    _name="panipat.supplier.return"
+    
