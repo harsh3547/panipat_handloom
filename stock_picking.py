3fcc6877ace3d2c80854dc0f4923da8b3728cfd7
harsh jain <harsh.jain@jginfosystems.com>
2016-01-22 18:40:42 +0530

    [IMP] transferring origin from picking (=sale_order) to invoice from picking

--- stock_picking.py
+++ stock_picking.py
@@ -54,4 +54,17 @@ class stock_picking(models.Model):
                     }
     
     
+class stock_invoice_onshipping(models.TransientModel):
+    _inherit="stock.invoice.onshipping"
+    
+    @api.multi
+    def create_invoice(self):
+        print self._context
+        res=super(stock_invoice_onshipping, self).create_invoice()
+        print "=========in panipat handloom======",res
+        inv_obj=self.env['account.invoice'].browse(res)
+        origin_picking=self.env['stock.picking'].browse(self._context.get('active_id')).origin
+        if origin_picking:
+            inv_obj.write({'origin':origin_picking+":"+inv_obj.origin})
+        return res
     
