f229b8131d2bcc4c164dfde994be6459a0978e44
harsh jain <harsh3547@gmail.com>
2017-08-07 14:08:01 +0530

    [IMP] added GST,HSN code feature in account invoice report

--- custom_report/report/report_invoice.py
+++ custom_report/report/report_invoice.py
@@ -6,18 +6,43 @@ from openerp.report import report_sxw
 from openerp.tools.translate import _
 from openerp.osv import osv
 from openerp.tools.amount_to_text_en import amount_to_text
+from openerp.tools.float_utils import float_round, float_compare
+
 import string
 
 class report_account_invoice_customer(report_sxw.rml_parse):
 
     def __init__(self, cr, uid, name, context=None):
-        print "=-=-=----------666666666666666666666666666"
+        print "=-=-=----------666666666666666666666666666",context
         super(report_account_invoice_customer, self).__init__(cr, uid, name, context=context)
         self.localcontext.update( {
             'time': time,
-            'amount_in_word': self._amount_to_text
+            'amount_in_word': self._amount_to_text,
+            'round_off_fmt':self._round_off_fmt,
+            'tax_amt':self._get_tax_value,
         })
         self.context = context
+
+    def _get_tax_value(self,tax):
+        cr=self.cr
+        uid=self.uid
+        invoice_id=self.context.get('active_ids',[False])[0]
+        inv_obj=self.pool.get("account.invoice").browse(cr,uid,invoice_id,context=self.context)
+        tax_amt={}
+        local_gst=0.0
+        tax_amt['CGST']=0
+        tax_amt['SGST']=0
+        tax_amt['IGST']=0
+        for res in inv_obj.tax_line:
+            if ('cgst' in res.name.lower()) or ('sgst' in res.name.lower()):
+                local_gst=res.amount
+            elif 'igst' in res.name.lower():
+                tax_amt['IGST']=res.amount
+            break
+        tax_amt['CGST']=tax_amt['SGST']=local_gst/2.0
+        prec=self.pool.get('decimal.precision').precision_get(self.cr, self.uid, 'Account')
+        final_amt="%.{0}f".format(prec)%tax_amt[tax]
+        return final_amt
 
     def _amount_to_text(self, amount, currency_id, context=None):
         # Currency complete name is not available in res.currency model
@@ -39,6 +64,12 @@ class report_account_invoice_customer(report_sxw.rml_parse):
             amout_str = string.replace(amout_str, 'Cents', 'Paisa')
             amout_str = string.replace(amout_str, 'Cent', 'Paisa')
         return amout_str
+
+    def _round_off_fmt(self,round_off,context=None):
+        prec=self.pool.get('decimal.precision').precision_get(self.cr, self.uid, 'Account')
+        round_off_fmt = "%.{0}f".format(prec)%round_off
+        return round_off_fmt
+        
 
 class customer_report_invoice(osv.AbstractModel):
     _name = 'report.panipat_handloom.customer_report_invoice'
--- custom_report/res_company.py
+++ custom_report/res_company.py
@@ -8,12 +8,16 @@ class res_company(models.Model):
     company_vat_tin = fields.Char("Company's VAT TIN")
     company_cst_no = fields.Char("Company's CST No.")
     company_pan_no = fields.Char("Company's PAN")
+    company_gst_tin = fields.Char("Company's GSTIN")
+    report_header_text = fields.Text("Report header Text")
     
     
 class res_partner(models.Model):
     _inherit = "res.partner"
 
     buyer_vat_tin = fields.Char("VAT TIN")
+    buyer_gst_tin = fields.Char("GSTIN")
+
     
 class account_invoice(models.Model):
     _inherit = "account.invoice"
@@ -26,5 +30,5 @@ class account_invoice(models.Model):
         assert len(self) == 1, 'This option should only be used for a single id at a time.'
         self.sent = True
         if self.type == 'out_invoice':
-            return self.env['report'].get_action(self, 'custom_report.customer_report_invoice')
-        return self.env['report'].get_action(self, 'account.report_invoice')
+            return self.env['report'].get_action(self, 'panipat_handloom.customer_report_invoice')
+        return True
